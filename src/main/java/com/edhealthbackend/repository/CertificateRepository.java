417e4f05e65e33125e0e8ce1ea5cd5995280f3d4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-04 15:30:38 +0200

    remove unwanted space

--- src/main/java/com/edhealthbackend/repository/CertificateRepository.java
+++ src/main/java/com/edhealthbackend/repository/CertificateRepository.java
@@ -1,11 +1,5 @@
 package com.edhealthbackend.repository;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.edhealthbackend.model.Certificate;
-import com.edhealthbackend.model.Training;
 public interface CertificateRepository extends JpaRepository<Certificate,Long>{
-
-    List<Certificate> findAllByTraining(Training trainingId);
 }
