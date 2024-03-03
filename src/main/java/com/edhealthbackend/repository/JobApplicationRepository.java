124a63cc5352387d33f7162761549ec69c43a766
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-03 15:22:48 +0200

    add find all by student and status

--- src/main/java/com/edhealthbackend/repository/JobApplicationRepository.java
+++ src/main/java/com/edhealthbackend/repository/JobApplicationRepository.java
@@ -1,9 +1,16 @@
 package com.edhealthbackend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edhealthbackend.model.JobApplication;
+import com.edhealthbackend.model.Student;
 
 public interface JobApplicationRepository extends JpaRepository<JobApplication,Long>{
+
+    Page<JobApplication> findAllByStudentAndStatus(Student student, String status, PageRequest of);
+
+    JobApplication findByStudent(Student student);
 
 }
