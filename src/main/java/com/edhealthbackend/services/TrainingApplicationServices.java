572ba4d6f100549c8bf7da3b9fe6c86967674afb
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-03 15:23:42 +0200

    remove un used import

--- src/main/java/com/edhealthbackend/services/TrainingApplicationServices.java
+++ src/main/java/com/edhealthbackend/services/TrainingApplicationServices.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
