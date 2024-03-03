4aa36e1857ae1061c380e6edd772f3e07c2252b9
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-03 15:23:14 +0200

    add find student job application list

--- src/main/java/com/edhealthbackend/services/JobApplicationServices.java
+++ src/main/java/com/edhealthbackend/services/JobApplicationServices.java
@@ -1,11 +1,21 @@
 package com.edhealthbackend.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.edhealthbackend.interfaces.DefaultRepositoryMethod;
+import com.edhealthbackend.model.Job;
 import com.edhealthbackend.model.JobApplication;
+import com.edhealthbackend.model.Student;
+import com.edhealthbackend.model.gql.InputDefs.PaginationInput;
+import com.edhealthbackend.model.gql.pagination.JobApplicationPage;
 import com.edhealthbackend.repository.JobApplicationRepository;
 
 @Service
@@ -16,7 +26,9 @@ public class JobApplicationServices extends DefaultRepositoryMethod<JobApplicati
 
   @Autowired
   private JobApplicationRepository jobApplicationRepository;
-
+  @Autowired
+  private StudentServices studentServices;
+  @Autowired private JobServices jobServices;
   @Override
   public String deleteById(Long id) {
     try {
@@ -36,5 +48,33 @@ public class JobApplicationServices extends DefaultRepositoryMethod<JobApplicati
             || j.getJob().getDescription().toLowerCase().equals(joString)
             || j.getJob().getTitle().toLowerCase().equals(joString)))
         .toList();
+  }
+
+  public JobApplicationPage findStudentJobApplicationList(long studentId, String status, PaginationInput in) {
+    Student student = studentServices.findById(studentId);
+    Page<JobApplication> page = jobApplicationRepository.findAllByStudentAndStatus(student, status,
+        PageRequest.of(in.getPageNumber(), in.getPageSize(), Sort.by(in.getSort())));
+    return new JobApplicationPage(page.getContent(), page.getNumber(), page.getTotalPages(), page.getSize());
+  }
+  public ResponseEntity<String> registerJobApplication(long studentId, long jobId, String status) {
+    Job job = jobServices.findById(jobId);
+    Student student = studentServices.findById(studentId);
+    if (job == null || student == null)
+      return new ResponseEntity<>("Please specify correct job", HttpStatus.METHOD_NOT_ALLOWED);
+    else {
+      // find if job application is already exist
+      JobApplication studentApplication = jobApplicationRepository.findByStudent(student);
+      if (studentApplication == null) {
+        JobApplication jobApplication = this
+            .saveOrUpdate(new JobApplication(0, job, student, LocalDateTime.now(), status));
+        return new ResponseEntity<>(
+            jobApplication.getStudent().getUser().getName() + " Your application has been received successful",
+            HttpStatus.OK);
+      } else {
+        studentApplication.setStatus(status);
+        this.saveOrUpdate(studentApplication);
+        return new ResponseEntity<>("Application " + status + " successful", HttpStatus.OK);
+      }
+    }
   }
 }
