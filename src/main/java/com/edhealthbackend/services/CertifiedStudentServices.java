b3e08af0cd3d594883492a11791e2748d27b90f3
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-04 20:52:31 +0200

    create findStudentCertificates

--- src/main/java/com/edhealthbackend/services/CertifiedStudentServices.java
+++ src/main/java/com/edhealthbackend/services/CertifiedStudentServices.java
@@ -2,10 +2,16 @@ package com.edhealthbackend.services;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import com.edhealthbackend.interfaces.DefaultRepositoryMethod;
 import com.edhealthbackend.model.CertifiedStudent;
+import com.edhealthbackend.model.Student;
+import com.edhealthbackend.model.gql.InputDefs.PaginationInput;
+import com.edhealthbackend.model.gql.pagination.CertifiedStudentPage;
 import com.edhealthbackend.repository.CertifiedStudentRepository;
 
 
@@ -14,12 +20,12 @@ public class CertifiedStudentServices extends DefaultRepositoryMethod<CertifiedS
 public CertifiedStudentServices(JpaRepository<CertifiedStudent, Long> jpaRepository) {
         super(jpaRepository);
     }
-@Autowired private CertifiedStudentRepository tCertificateRepository;
+@Autowired private CertifiedStudentRepository certifiedStudentRepository;
 @Override
 public String deleteById(Long id) {
    try {
     CertifiedStudent trainingCertificate=this.findById(id);
-    tCertificateRepository.delete(trainingCertificate);
+    certifiedStudentRepository.delete(trainingCertificate);
     return trainingCertificate.getCertificate().getTitle()+" Certificate rejected";
    } catch (Exception e) {
     return "Certificate not found";
@@ -29,12 +35,18 @@ public String deleteById(Long id) {
 @Override
 public List<CertifiedStudent> search(String search) {
     String searcString=search.toLowerCase();
-  return tCertificateRepository.findAll().
+  return certifiedStudentRepository.findAll().
   stream().
   filter(tc->(
 tc.getCertificate().getDescription().toLowerCase().equals(searcString)
 ||tc.getTimeStamp().toString().equals(searcString)
   )).
   toList();
+}
+@Autowired private StudentServices studentServices;
+public CertifiedStudentPage findStudentCertificates(long studentId, PaginationInput input) {
+    Student student=studentServices.findById(studentId);
+    Page<CertifiedStudent>page=certifiedStudentRepository.findAllByStudent(student,PageRequest.of(input.getPageNumber(), input.getPageSize(),Sort.by(input.getSort())));
+    return new CertifiedStudentPage(page.getContent(), page.getNumber(), page.getTotalPages(), page.getSize());
 }
 }
