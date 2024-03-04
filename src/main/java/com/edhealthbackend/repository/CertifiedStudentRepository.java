2a810c02476991962b9d0b6f4ef224d9b7d8b83a
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-04 20:52:10 +0200

    create findAllByStudent

--- src/main/java/com/edhealthbackend/repository/CertifiedStudentRepository.java
+++ src/main/java/com/edhealthbackend/repository/CertifiedStudentRepository.java
@@ -1,8 +1,13 @@
 package com.edhealthbackend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edhealthbackend.model.CertifiedStudent;
+import com.edhealthbackend.model.Student;
 public interface CertifiedStudentRepository extends JpaRepository<CertifiedStudent,Long>{
+
+    Page<CertifiedStudent> findAllByStudent(Student student, PageRequest of);
 
 }
