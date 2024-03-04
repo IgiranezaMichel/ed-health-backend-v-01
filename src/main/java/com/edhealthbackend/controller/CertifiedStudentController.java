6e44d2527eba29090131d663128d37ebdcea6dc1
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-04 21:11:58 +0200

    add giveCertificateToStudent method

--- src/main/java/com/edhealthbackend/controller/CertifiedStudentController.java
+++ src/main/java/com/edhealthbackend/controller/CertifiedStudentController.java
@@ -2,9 +2,12 @@ package com.edhealthbackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import com.edhealthbackend.model.gql.InputDefs.CertifiedStudentInput;
 import com.edhealthbackend.model.gql.InputDefs.PaginationInput;
 import com.edhealthbackend.model.gql.pagination.CertifiedStudentPage;
 import com.edhealthbackend.services.CertifiedStudentServices;
@@ -12,6 +15,10 @@ import com.edhealthbackend.services.CertifiedStudentServices;
 @Controller
 public class CertifiedStudentController {
 @Autowired private CertifiedStudentServices certifiedStudentServices;
+@MutationMapping
+public ResponseEntity<String> giveCertificateToStudent(@Argument(name ="input")CertifiedStudentInput studentCertificate){
+    return certifiedStudentServices.saveStudentCertificate(studentCertificate);
+}
 @QueryMapping
 public CertifiedStudentPage findStudentCertificates(@Argument(name = "studentId")long studentId,PaginationInput input){
     return certifiedStudentServices.findStudentCertificates(studentId,input);
