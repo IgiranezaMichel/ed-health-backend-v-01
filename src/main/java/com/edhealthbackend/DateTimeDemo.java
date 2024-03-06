497d3da6917818f0c37a33c47a915f501e1d9449
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-06 12:16:58 +0200

    conversion

--- src/main/java/com/edhealthbackend/DateTimeDemo.java
+++ src/main/java/com/edhealthbackend/DateTimeDemo.java
@@ -1,14 +1,7 @@
 package com.edhealthbackend;
-
-import java.util.Random;
-
 public class DateTimeDemo {
 public static void main(String[] args) {
-    Random random=new Random();
-    long raString=random.nextLong(2000l);
-
-    // LocalDateTime today=LocalDateTime.parse("2024-02-17T09:00");
-    System.out.println(raString);
+ 
     // String pattern = "#,####";
 // DecimalFormat decimalFormat = new DecimalFormat(pattern);
 }
