4ab258384e175de9ff5e2562164b4818f1d75f18
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-06 14:26:16 +0200

    remove constructor

--- src/main/java/com/edhealthbackend/model/gql/InputDefs/CertificateInput.java
+++ src/main/java/com/edhealthbackend/model/gql/InputDefs/CertificateInput.java
@@ -1,7 +1,6 @@
 package com.edhealthbackend.model.gql.InputDefs;
 
 import java.time.LocalDateTime;
-
 import com.edhealthbackend.model.Certificate;
 
 import lombok.Getter;
@@ -9,13 +8,5 @@ import lombok.Setter;
 
 public class CertificateInput extends Certificate{
 @Getter @Setter private long trainingId;
-public CertificateInput(long id,String title,String description,String signature,String stamp,LocalDateTime timeStamp,long trainingId){
-    this.setId(id);
-    this.setTitle(title);
-    this.setDescription(description);
-    this.setSignature(signature);
-    this.setStamp(stamp);
-    this.setTimeStamp(timeStamp);
-    this.trainingId=trainingId;
-}
+@Getter @Setter private long accountHolderId;
 }
