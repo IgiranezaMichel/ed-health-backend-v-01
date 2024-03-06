b6c81b580957c6fce777bb7f0edcb92c699bd0e6
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-06 14:25:54 +0200

    add lob

--- src/main/java/com/edhealthbackend/model/Certificate.java
+++ src/main/java/com/edhealthbackend/model/Certificate.java
@@ -1,17 +1,19 @@
 package com.edhealthbackend.model;
 
 import java.time.*;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.*;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -22,21 +24,26 @@ public class Certificate {
     private long id;
     private String title;
     private String description;
-    private String signature;
-    private String stamp;
+    @Lob
+    private String userSignature;
+    @Lob
+    private String hospitalStamp;
+    @ManyToOne(cascade = CascadeType.ALL,targetEntity = AccountHolder.class)
+    private AccountHolder accountHolder;
     private LocalDateTime timeStamp;
     @ManyToOne
     private Training training;
 
     public Certificate(long id, String title, String description, String signature, String stamp,
-            LocalDateTime now, Training training) {
+            LocalDateTime now, Training training,AccountHolder accountHolder) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.signature = signature;
-        this.stamp = stamp;
+        this.userSignature = signature;
+        this.hospitalStamp = stamp;
         this.timeStamp = now;
         this.training = training;
+        this.accountHolder=accountHolder;
     }
     @OneToMany(mappedBy = "certificate")
     private List<CertifiedStudent> certifiedStudentList;
