537f340568fa7eca9a8f680556296dfe9820fcc1
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-25 09:24:12 +0200

    remove training requirement

--- src/main/java/com/edhealthbackend/repository/TrainingRequirementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edhealthbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edhealthbackend.model.TrainingRequirement;
-
-public interface TrainingRequirementRepository extends JpaRepository<TrainingRequirement,Long> {
-
-}
