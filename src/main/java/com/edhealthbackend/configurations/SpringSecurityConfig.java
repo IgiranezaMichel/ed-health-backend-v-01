0390be55de0408d9f933ab857a639853b218b632
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-29 15:51:35 +0200

    add spring security

---
+++ src/main/java/com/edhealthbackend/configurations/SpringSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.edhealthbackend.configurations;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.authentication.AuthenticationProvider;
+// import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.web.SecurityFilterChain;
+
+// @Configuration
+public class SpringSecurityConfig{
+// public SecurityFilterChain webSecurity(HttpSecurity http) throws Exception {
+//     return http
+//         .csrf(csrf->csrf.disable())
+//         .authorizeHttpRequests(auth->{
+//            auth.requestMatchers("/login").permitAll();
+//            auth.anyRequest().authenticated();
+//         })
+//         .formLogin(login->{
+//             login.loginPage("/login").permitAll(); 
+//             login.failureUrl("/login?error");
+//         }).build();        
+//     }
+//     @Bean
+//     public AuthenticationProvider authenticationProvider() {
+    //     DaoAuthenticationProvider provider =new DaoAuthenticationProvider();
+    //     provider.setUserDetailsService(userDetailsService);
+    //     provider.setPasswordEncoder(new BCryptPasswordEncoder());
+    //     return provider;
+
+    // }
+}
