0ea0b67faff4a97a28fbaa6f1cc12dc33e49eb70
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-29 08:53:36 +0200

    add super class

--- src/main/java/com/edhealthbackend/model/gql/InputDefs/SchoolAdminInput.java
+++ src/main/java/com/edhealthbackend/model/gql/InputDefs/SchoolAdminInput.java
@@ -1,11 +1,20 @@
 package com.edhealthbackend.model.gql.InputDefs;
 
+import java.time.LocalDateTime;
+
+import com.edhealthbackend.enums.Status;
+import com.edhealthbackend.model.AccountHolder;
+import com.edhealthbackend.model.School;
 import com.edhealthbackend.model.SchoolAdmin;
 
 import lombok.Getter;
 import lombok.Setter;
 
 public class SchoolAdminInput extends SchoolAdmin{
+    public SchoolAdminInput(long id, School school, AccountHolder user, String position, LocalDateTime startingDate,
+            LocalDateTime endDate, Status status) {
+        super(id, school, user, position, startingDate, endDate, status);
+    }
     @Getter @Setter private long schoolId;
     @Getter @Setter private long userId;
 }
