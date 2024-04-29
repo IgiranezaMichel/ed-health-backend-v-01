405bd5bb4122ecd9a6506208175dbec4fb468f50
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-29 08:53:57 +0200

    add constructor

--- src/main/java/com/edhealthbackend/model/supperClass/Admin.java
+++ src/main/java/com/edhealthbackend/model/supperClass/Admin.java
@@ -5,9 +5,13 @@ import com.edhealthbackend.enums.Status;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @MappedSuperclass
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Admin {
 private String position;
 private LocalDateTime startingDate;
